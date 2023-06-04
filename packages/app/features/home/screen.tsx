795a32c59dc81bc5023d08ab3c274e6646f9d796
Huynh Duc Dung <dunghd.it@gmail.com>
2023-06-04 23:00:23 +0800

    :lipstick: Show language in grid view

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -1,14 +1,15 @@
-import { useRouter } from 'next/router'
 import React from 'react'
+import { useRouter } from 'solito/router'
 
-import { H1, Image, YStack } from '@productsway/ui'
+import { Anchor, Grid, H1, Image, YStack } from '@productsway/ui'
 
 const languages = [
-  { name: 'English', flag: 'https://flagcdn.com/16x12/gb.png', code: 'en' },
-  { name: 'Español', flag: 'https://flagcdn.com/16x12/es.png', code: 'es' },
-  { name: 'Français', flag: 'https://flagcdn.com/16x12/fr.png', code: 'fr' },
-  { name: 'Deutsch', flag: 'https://flagcdn.com/16x12/de.png', code: 'de' },
-  { name: 'Italiano', flag: 'https://flagcdn.com/16x12/it.png', code: 'it' },
+  { name: 'English', flag: 'https://flagcdn.com/64x48/gb.png', code: 'en' },
+  { name: 'Vietnam', flag: 'https://flagcdn.com/64x48/vn.png', code: 'vn' },
+  { name: 'Español', flag: 'https://flagcdn.com/64x48/es.png', code: 'es' },
+  { name: 'Français', flag: 'https://flagcdn.com/64x48/fr.png', code: 'fr' },
+  { name: 'Deutsch', flag: 'https://flagcdn.com/64x48/de.png', code: 'de' },
+  { name: 'Italiano', flag: 'https://flagcdn.com/64x48/it.png', code: 'it' },
 ]
 
 export function HomeScreen() {
@@ -17,16 +18,19 @@ export function HomeScreen() {
   const handleLanguagePress = (code: string) => {
     router.push(`/language/${code}`)
   }
-
   return (
-    <YStack f={1} p="$4" space>
-      <H1 ta="center">Select a language:</H1>
-      {languages.map((language) => (
-        <YStack key={language.code} p="$4" onPress={() => handleLanguagePress(language.code)}>
-          <Image source={{ uri: language.flag }} width={16} height={12} />
-          <H1>{language.name}</H1>
-        </YStack>
-      ))}
+    <YStack f={1} p="$8" space="$4" h="100%">
+      <H1 ta="center">Welcome to Learn2Write4Kids!</H1>
+      <YStack f={1} p="$8" space="$4" h="100%" jc="center">
+        <Grid gap="$4" itemMinWidth={200}>
+          {languages.map((language) => (
+            <Anchor key={language.code} onPress={() => handleLanguagePress(language.code)}>
+              <Image source={{ uri: language.flag }} width={64} height={48} />
+              <H1>{language.name}</H1>
+            </Anchor>
+          ))}
+        </Grid>
+      </YStack>
     </YStack>
   )
 }
